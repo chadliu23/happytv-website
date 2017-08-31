90ed421ba31eec175db9f3fbc33b214f4fb7c2ac
Chad Liu <chadliu23@happytv.com.tw>
2017-08-31 14:51:19 +0800

    try to fix again

--- js/login-digital-taipei-2017.js
+++ js/login-digital-taipei-2017.js
@@ -37,15 +37,22 @@
           $.ajax({
             type: 'POST',
             headers: {
-              "accesskey": "accessKey_k46zs4fyf4rbajev6px4384uztxhd3hrtdmu2btgzubtrpz9cpsnrnfqfhruyshp",
+              "accesskey": "accessKey_eb3604bd21a3176806f29607d47b069f17956cba",
             },
             dataType: 'json',
             data: { "fb_id": data.id, "fb_token": response.authResponse.accessToken },
             url: 'https://api.happytv.com.tw/happytvmember/login?source=facebook'
           }).done((data) => {
-            succuessLogin(data);
+            if (data.retCode === 0){
+              succuessLogin(data);
+            }else{
+              console.log('Login fail ' + data.retMessage);
+              FB.logout(function(response) {
+              });
+            }
+            
           }).fail((data) =>{
-            bootstrap_alert.warning('This Facebook account as not register.');
+            onsole.log('Login fail ');
             FB.logout(function(response) {
             });
           });
@@ -108,51 +115,7 @@ function succuessLogin(data){
   checkstatus();
 }
 
-if ($('#loginbutton')){
-  $('#loginbutton').on('click', function(event){
-    var email = $('#email').val();
-    if (email == ''){
-      return;
-    }
-    var password = $('#password').val();
-    if (!validateEmail(email)){
-      bootstrap_alert.warning('Please input correct email');
-      event.preventDefault();
-      return;
-    }
-    if (password === '' | password === undefined){
-      bootstrap_alert.warning('Please input password');
-      event.preventDefault();
-      return;
-    }
-    if (!$('#readChecked').is(":checked")){
-      bootstrap_alert.warning('Please Check the checkbox');
-      event.preventDefault();
-      return;
-    }
-    password = CryptoJS.SHA256('h@ppytvXXX' + password);
-    console.log(password.toString(CryptoJS.enc.Hex));
-    $.ajax({
-      type: 'POST',
-      headers: {
-        "accesskey": "accessKey_eb3604bd21a3176806f29607d47b069f17956cba"
-      },
-      dataType: 'json',
-      data: { "email": email, "password": password.toString(CryptoJS.enc.Hex) },
-      url: 'https://api-stage.happytv.com.tw/happytvmember/login?source=email'
-    }).done((data) => {
-      if (data.retCode === 0){
-        succuessLogin(data);
-      }else{
-        console.log('log in fail ' + data.retMessage);
-      }
-      
-    }).fail((data) =>{
-      console.log('log in fail');
-    });
-    event.preventDefault();
-  });
-}
+
 
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
