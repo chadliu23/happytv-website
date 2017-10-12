f7e358e1c8c374f7c26adf8b3049afc97987621b
Chad Liu <chadliu23@happytv.com.tw>
2017-10-12 16:33:39 +0800

    Add start button

--- js/lottary.js
+++ js/lottary.js
@@ -172,6 +172,7 @@ $(document).ready(function(){
       $('#promotion-code-area').css('display', 'block');
       $('#start-button').css('display', 'none');
     }).fail((data) =>{
+      $('#promotion-code-area').css('display', 'block');
       $('#start-button').css('display', 'none');
       console.log(data);
     });
