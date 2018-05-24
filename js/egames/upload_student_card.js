0f96881377b4ffc711c6d769ab7ceb12572eea58
Chad Liu <chadliu23@happytv.com.tw>
2018-05-25 04:41:29 +0800

    Add verify for photo update

--- js/egames/upload_student_card.js
+++ js/egames/upload_student_card.js
@@ -98,7 +98,29 @@ function putInData(data) {
 
 };
 
-
+var _validFileExtensions = [".jpg", ".png"];    
+function ValidateSingleInput(oInput) {
+    if (oInput.type == "file") {
+        var sFileName = oInput.value;
+         if (sFileName.length > 0) {
+            var blnValid = false;
+            for (var j = 0; j < _validFileExtensions.length; j++) {
+                var sCurExtension = _validFileExtensions[j];
+                if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                    blnValid = true;
+                    break;
+                }
+            }
+             
+            if (!blnValid) {
+                alert("請選擇" + _validFileExtensions.join("或")+ "的圖片");
+                oInput.value = "";
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 $("#upload-form").submit(function (e) {
   e.preventDefault();
