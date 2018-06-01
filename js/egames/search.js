588b3ebacf83d8a25222008f0b4fe68b67a2c51c
Akon Kang <akon825@happytv.com.tw>
2018-06-01 18:04:48 +0800

    add egame search page function

---
+++ js/egames/search.js
@@ -0,0 +1,93 @@
+function setLoadingBlock() {
+  $.blockUI({
+    message: 'loading...',
+    css: {
+      border: 'none',
+      padding: '15px',
+      backgroundColor: '#000',
+      '-webkit-border-radius': '10px',
+      '-moz-border-radius': '10px',
+      opacity: .5,
+      color: '#fff',
+      message: 'Loading...'
+    }
+  });
+}
+
+var teamIdMap = {}
+
+$(document).ready(function(){
+	$.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: happyApiHost + '/api/v3/egame/2018-01/allTeam'
+    }).done(function(data)  {
+    	console.log(data)
+      if (data.result.length === 0) {
+        return
+      }
+
+      return putInTeam(data.result)
+    }).fail(function(data) {
+      console.log('失敗')
+    });
+
+    function putInTeam(teams) {
+    	var availableTags = []
+    	teams.forEach(function(team, i){
+    		availableTags.push(team.team_name)
+    		teamIdMap[team.team_name] = team.team_id
+    	})
+
+	    $( "#team_name_search" ).autocomplete({
+	      source: availableTags
+	    });
+    }
+})
+
+$(document).on('change', '#team_name_search',  function(e) {
+	$("#search-form").submit();
+})
+$(document).on('click', '.btn_search',  function(e) {
+	$("#search-form").submit();
+})
+$(document).on('submit', '#search-form',  function(e) {
+//$("#search-form").submit(function (e) {
+	e.preventDefault();
+
+	var team_name = $( "#team_name_search" ).val()
+	var team_id = teamIdMap[team_name]
+
+	if(!team_id) {
+		return
+	}
+
+	setLoadingBlock()
+
+  	$.ajax({
+		type: 'GET',
+		dataType: 'json',
+		url: happyApiHost + '/api/v3/egame/2018-01/search/team/' + team_id
+	}).done(function(data)  {
+			console.log(data)
+		if (data.result.length === 0) {
+			$.unblockUI()
+			return
+		}
+
+		$("#team_name_p").html(data.result[0].team_name)
+
+		for(var i=0; i<=6; i++) {
+			if(data.result[i]) {
+				$("#summoner_" + i + "_p .name").html(data.result[i].summoner)
+				$("#summoner_" + i + "_p .school").html(data.result[i].school_name)
+			}
+		}
+
+	  	$.unblockUI()
+	}).fail(function(data) {
+	  	console.log('失敗')
+	  	$.unblockUI()
+	});
+
+})
