82a11b462ea538c9ab1a5dd3e30696068c769d95
Akon Kang <akon825@happytv.com.tw>
2018-06-27 16:03:18 +0800

    fix game-made

--- js/egames/game-made.js
+++ js/egames/game-made.js
@@ -5,6 +5,7 @@ tmp.blue_id = ''
 tmp.blue_name = ''
 tmp.red_id = ''
 tmp.red_name = ''
+tmp.user_is_leader = false
 
 $(document).ready(function(){
   if (Cookies.get("member_token") !== undefined) {
@@ -43,7 +44,7 @@ function getGameMadePageData (data) {
       }
 
       // 上方標題
-      var teamDescript = '您的隊伍為「' + teamName + '」, 此輪對戰隊伍為「' + opponentTeamName + '」, 名單如下';
+      var teamDescript = '您的隊伍為「' + teamName + '」, 此輪對戰隊伍為「' + opponentTeamName + '」';
       $('#team-descript').html(teamDescript)
 
       // 敵隊名單
@@ -67,17 +68,17 @@ function getGameMadePageData (data) {
         // 第一次約戰
         if(!data.result.gameMade || !data.result.gameMade.first_date) {
           myCube = myCube + '<div class="lil-title">第一次約戰時間</div>' +
-          '<div><form id="form_date_made"><input type="datetime-local" name="made_time"><button class="btn_fighting" type="submit">送出</button></form></div>'
+          '<div><form id="form_date_made"><input id="date-pick" name="made_time"><button class="btn_fighting" type="submit">送出</button></form></div>'
           return
         }
 
         // 第一次約戰確認
         if (data.result.gameMade.first_date && data.result.gameMade.first_date_confirm === null) {
           if (data.result.gameMade.first_date_made_by === position) {
-            myCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>'
+            myCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD HH:mm:ss') + '</div>'
             return
           } else {
-            opCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>'
+            opCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD HH:mm:ss') + '</div>'
             myCube = '<div class="lil-title">第一次約戰時間確認</div>' +
               '<div><button id="btn_confirm_ok" class="btn_fighting">同意約戰時間</button></div>' +
               '<div><button id="btn_confirm_no" class="btn_fighting">無法配合該約戰時間</button></div>'
@@ -89,10 +90,10 @@ function getGameMadePageData (data) {
         if(data.result.gameMade.first_date_confirm === 1) {
           if (data.result.gameMade.first_date_made_by === position) {
             opCube = opCube + '<div class="lil-title">第一次約戰時間確認</div>' + '<div>『 對手同意 』</div>' + '<hr>'
-            myCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            myCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           } else {
             myCube = myCube + '<div class="lil-title">第一次約戰時間確認</div>' + '<div>『 您同意 』</div>' + '<hr>'
-            opCube = opCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            opCube = opCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           }
           return
         }
@@ -101,29 +102,34 @@ function getGameMadePageData (data) {
         if(data.result.gameMade.first_date_confirm === 0) {
           if (data.result.gameMade.first_date_made_by === position) {
             opCube = opCube + '<div class="lil-title">第一次約戰時間確認</div>' + '<div>『 對手不同意 』</div>' + '<hr>'
-            myCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            myCube = myCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           } else {
             myCube = myCube + '<div class="lil-title">第一次約戰時間確認</div>' + '<div>『 您不同意 』</div>' + '<hr>'
-            opCube = opCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            opCube = opCube + '<div class="lil-title">第一次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           }
         }
 
         // 加入第二次約戰input
-        if(!data.result.gameMade.second_date) {
+        if(!data.result.gameMade.second_date && data.result.gameMade.first_date_made_by !== position) {
           myCube = myCube + '<div class="lil-title">第二次約戰時間</div>' +
-          '<div><form id="form_date_made"><input type="datetime-local" name="made_time"><button class="btn_fighting" type="submit">送出</button></form></div>'
+          '<div><form id="form_date_made"><input id="date-pick" name="made_time"><button class="btn_fighting" type="submit">送出</button></form></div>'
+          return
+        } else if (!data.result.gameMade.second_date && data.result.gameMade.first_date_made_by === position) {
+          myCube = myCube + '<div class="lil-title">等待對手約戰</div>'
+          return
         }
 
         // 第二次約戰確認
         if (data.result.gameMade.second_date && data.result.gameMade.second_date_confirm === null) {
           if (data.result.gameMade.second_date_made_by === position) {
-            myCube = myCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>'
+            myCube = myCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.second_date).format('YYYY-MM-DD HH:mm:ss') + '</div>'
+            return
           } else {
-            opCube = opCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>'
+            opCube = opCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.second_date).format('YYYY-MM-DD HH:mm:ss') + '</div>'
             myCube = myCube + '<div class="lil-title">第二次約戰時間確認</div>' +
               '<div><button id="btn_confirm_ok" class="btn_fighting">同意約戰時間</button></div>' +
               '<div><button id="btn_confirm_no" class="btn_fighting">無法配合該約戰時間</button></div>'
-              return
+            return
           }
         }
 
@@ -131,10 +137,10 @@ function getGameMadePageData (data) {
         if(data.result.gameMade.second_date_confirm === 1) {
           if (data.result.gameMade.second_date_made_by === position) {
             opCube = opCube + '<div class="lil-title">第二次約戰時間確認</div>' + '<div>『 對手同意 』</div>' + '<hr>'
-            myCube = myCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            myCube = myCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.second_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           } else {
             myCube = myCube + '<div class="lil-title">第二次約戰時間確認</div>' + '<div>『 您同意 』</div>' + '<hr>'
-            opCube = opCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            opCube = opCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.second_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           }
 
           return
@@ -144,29 +150,33 @@ function getGameMadePageData (data) {
         if(data.result.gameMade.second_date_confirm === 0) {
           if (data.result.gameMade.second_date_made_by === position) {
             opCube = opCube + '<div class="lil-title">第二次約戰時間確認</div>' + '<div>『 對手不同意 』</div>' + '<hr>'
-            myCube = myCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            myCube = myCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.second_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           } else {
             myCube = myCube + '<div class="lil-title">第二次約戰時間確認</div>' + '<div>『 您不同意 』</div>' + '<hr>'
-            opCube = opCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            opCube = opCube + '<div class="lil-title">第二次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.second_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           }
         }
 
         // 加入第三次約戰input
-        if(!data.result.gameMade.third_date) {
+        if(!data.result.gameMade.third_date && data.result.gameMade.second_date_made_by !== position) {
           myCube = myCube + '<div class="lil-title">第三次約戰時間</div>' +
-          '<div><form id="form_date_made"><input type="datetime-local" name="made_time"><button class="btn_fighting" type="submit">送出</button></form></div>'
+          '<div><form id="form_date_made"><input id="date-pick" name="made_time"><button class="btn_fighting" type="submit">送出</button></form></div>'
+          return
+        } else if (!data.result.gameMade.third_date && data.result.gameMade.second_date_made_by === position) {
+          myCube = myCube + '<div class="lil-title">等待對手約戰</div>'
+          return
         }
 
         // 第三次約戰確認
         if (data.result.gameMade.third_date && data.result.gameMade.third_date_confirm === null) {
           if (data.result.gameMade.third_date_made_by === position) {
-            myCube = myCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>'
+            myCube = myCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.third_date).format('YYYY-MM-DD HH:mm:ss') + '</div>'
           } else {
-            opCube = opCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>'
+            opCube = opCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.third_date).format('YYYY-MM-DD HH:mm:ss') + '</div>'
             myCube = myCube + '<div class="lil-title">第三次約戰時間確認</div>' +
               '<div><button id="btn_confirm_ok" class="btn_fighting">同意約戰時間</button></div>' +
               '<div><button id="btn_confirm_no" class="btn_fighting">無法配合該約戰時間</button></div>'
-              return
+            return
           }
         }
 
@@ -174,10 +184,10 @@ function getGameMadePageData (data) {
         if(data.result.gameMade.third_date_confirm === 1) {
           if (data.result.gameMade.third_date_made_by === position) {
             opCube = opCube + '<div class="lil-title">第三次約戰時間確認</div>' + '<div>『 對手同意 』</div>' + '<hr>'
-            myCube = myCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            myCube = myCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.third_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           } else {
             myCube = myCube + '<div class="lil-title">第三次約戰時間確認</div>' + '<div>『 您同意 』</div>' + '<hr>'
-            opCube = opCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            opCube = opCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.third_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           }
 
           return
@@ -187,10 +197,10 @@ function getGameMadePageData (data) {
         if(data.result.gameMade.third_date_confirm === 0) {
           if (data.result.gameMade.third_date_made_by === position) {
             opCube = opCube + '<div class="lil-title">第三次約戰時間確認</div>' + '<div>『 對手不同意 』</div>' + '<hr>'
-            myCube = myCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            myCube = myCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.third_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           } else {
             myCube = myCube + '<div class="lil-title">第三次約戰時間確認</div>' + '<div>『 您不同意 』</div>' + '<hr>'
-            opCube = opCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.first_date).format('YYYY-MM-DD hh:mm:ss') + '</div>' + '<hr>'
+            opCube = opCube + '<div class="lil-title">第三次約戰時間</div><div>約戰時間：' + moment(data.result.gameMade.third_date).format('YYYY-MM-DD HH:mm:ss') + '</div>' + '<hr>'
           }
         }
 
@@ -214,16 +224,38 @@ function getGameMadePageData (data) {
       tmp.blue_name = data.result.schedule.blue_name
       tmp.red_id = data.result.schedule.red_id
       tmp.red_name = data.result.schedule.red_name
+      tmp.user_is_leader = data.result.is_leader
       if(data.result.gameMade) {
         tmp.game_made_id = data.result.gameMade.row_id
       }
 
       // jquery 塞資料
       $('.blue-cube .big-title').text('藍方隊伍：' + data.result.schedule.blue_name)
-      $('.red-cube .big-title').text('紅方隊伍：' + data.result.schedule.red_name)
+      $('.red-cube .big-title').text('紫方隊伍：' + data.result.schedule.red_name)
 
       $('.blue-cube').append(blueCube)
       $('.red-cube').append(redCube)
+
+      // 設定date time picker
+      //jQuery('#date-pick').datetimepicker();
+      var today = moment().format('MM.DD.YYYY')
+      $.datetimepicker.setLocale('zh-TW');
+      jQuery('#date-pick').datetimepicker({
+        datepicker: true,
+        allowTimes: [
+          '09:00', '09:15', '09:30', '09:45', '10:00', '10:15', '10:30', '10:45', '11:00',
+          '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00',
+          '19:00', '19:15', '19:30', '19:45', '20:00', '20:15', '20:30', '20:45', '21:00',
+          '21:15', '21:30', '21:45', '22:00', '22:15', '22:30', '22:45', '23:00', '23:15',
+          '23:30', '23:45', '00:00'
+        ],
+        disabledWeekDays: [1],
+        disabledDates: [today],
+        formatDate: 'm.d.Y',
+        theme:'dark',
+        mask:true,
+      });
+
     } else {
       alert('尚無對戰資料')
     }
@@ -237,6 +269,10 @@ function getGameMadePageData (data) {
 
 $(document).on('click', '#btn_confirm_ok', function(e) {
   e.preventDefault();
+
+  if(!tmp.user_is_leader) {
+    return alert('只有隊長能夠使用此功能')
+  }
 
   var r = confirm("確定接受此約戰時間嗎?");
   if (r != true) {
@@ -267,6 +303,10 @@ $(document).on('click', '#btn_confirm_ok', function(e) {
 $(document).on('click', '#btn_confirm_no', function(e) {
   e.preventDefault();
 
+  if(!tmp.user_is_leader) {
+    return alert('只有隊長能夠使用此功能')
+  }
+
   var r = confirm("確定拒絕此約戰時間嗎?");
   if (r != true) {
     return
@@ -285,7 +325,10 @@ $(document).on('click', '#btn_confirm_no', function(e) {
     data: data,
     url: happyApiHost + '/api/v3/egame/2018-01/gameMade/dateConfirm/' + tmp.game_made_id
   }).done(function(data)  {
-    console.log(data)
+    if(data.retMessage === 'date invalid') {
+      alert('因約戰的時間必須大於今天, 系統已取消這筆約戰, 請再重新操作')
+    }
+
     window.location.reload();
   }).fail(function(xhr, textStatus, error)  {
     console.log(xhr, error)
@@ -295,6 +338,32 @@ $(document).on('click', '#btn_confirm_no', function(e) {
 
 $(document).on('submit', '#form_date_made', function(e) {
   e.preventDefault();
+
+  if(!tmp.user_is_leader) {
+    return alert('只有隊長能夠使用此功能')
+  }
+
+  var date = $(this).children('input[name="made_time"]').val()
+  date = moment(date, 'YYYY/MM/DD HH:mm')
+
+  if(date.day() === 1) {
+    return alert('※不可選擇星期一')
+  }
+
+  if(['9', '10', '11', '15', '16', '17', '19', '20', '21', '22', '23', '0'].indexOf(date.hours().toString()) === -1) {
+    return alert('※時段分別為：早【09:00-11:00】、中【15:00-17:00】、晚【19:00-24:00】')
+  }
+
+  if(['0', '15', '30', '45'].indexOf(date.minutes().toString()) === -1) {
+    return alert('※請選擇0, 15, 30, 45這些分鐘')
+  }
+
+  var todayMoment = moment(moment().format('YYYY/MM/DD'), 'YYYY/MM/DD')
+  var diffToday = todayMoment.diff(moment(date.format('YYYY/MM/DD'), 'YYYY/MM/DD'), 'days')
+
+  if(diffToday > -1) {
+    return alert('※日期請選擇大於今日的時間')
+  }
 
   var r = confirm("確定送出此約戰時間嗎?");
   if (r != true) {
@@ -324,8 +393,14 @@ $(document).on('submit', '#form_date_made', function(e) {
     dataType: 'json',
     data: data,
     url: happyApiHost + '/api/v3/egame/2018-01/gameMade/dateMade'
-  }).done(function(data)  {
-    console.log(data)
+  }).done(function(result)  {
+    if(result.retMessage === 'game made fail cause triple data in same hour') {
+      var startTime =  moment(new Date(data.date)).format('YYYY/MM/DD HH') + ':00'
+      var endTime =  moment(new Date(data.date)).add(1, 'hours').format('YYYY/MM/DD HH') + ':00'
+      alert('此約戰時間' + startTime + '~' + endTime + '已有三組以上約戰賽事排定轉播, 請選擇其他時段')
+    }
+
+    console.log(result)
     window.location.reload();
   }).fail(function(xhr, textStatus, error)  {
     console.log(xhr, error)
